37f208deaa8e51f32633f54e5fd4bab99eb469d0
Joe Trite <joetrite@gmail.com>
2016-07-23 12:44:32 -0400

    updates

--- scripts/upload_to_cloud.sh
+++ scripts/upload_to_cloud.sh
@@ -1,4 +1,3 @@
+rsync --remove-source-files -azv -e 'ssh -i /app/scout/conf/scout.pem' /app/scout/data/*.jpg ec2-user@ec2-54-84-169-92.compute-1.amazonaws.com:/data/
 
-rsync --remove-source-files -azv -e 'ssh -i /app/scout/conf/scout.pem' /app/scout/data/* ec2-user@ec2-54-84-169-92.compute-1.amazonaws.com:/data/
-
-rsync --remove-source-files -azv -e 'ssh -i /app/scout/conf/scout.pem' /app/scout/logs/* ec2-user@ec2-54-84-169-92.compute-1.amazonaws.com:/logs/
+rsync --remove-source-files -azv -e 'ssh -i /app/scout/conf/scout.pem' /app/scout/logs/*.log ec2-user@ec2-54-84-169-92.compute-1.amazonaws.com:/logs/
